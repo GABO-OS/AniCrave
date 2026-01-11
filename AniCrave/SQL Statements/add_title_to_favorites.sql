de620777af5763b807029ae6b31bd0566b0e488c
Gab <gabrielabordo07@gmail.com>
2026-01-11 21:07:48 +0800

    Delete AniCrave/SQL Statements directory

--- AniCrave/SQL Statements/add_title_to_favorites.sql
+++
@@ -1,7 +0,0 @@
--- Add anime_title column to favorites table
-ALTER TABLE favorites ADD COLUMN anime_title VARCHAR(255) AFTER anime_id;
-
--- Populate experimental anime_title column from existing anime data
-UPDATE favorites f 
-JOIN anime a ON f.anime_id = a.id 
-SET f.anime_title = a.title;
